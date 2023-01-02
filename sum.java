90d1f93f9a28a4a9e3267d28cd42ff280f0cc789
Tharsini Karunanithy <77621418+tharsh1509@users.noreply.github.com>
2023-01-02 14:55:43 +0530

    Update sum.java

--- sum.java
+++ sum.java
@@ -2,7 +2,7 @@ class sum{
     public static void main(String args []){
         int a=3;
         int b=8;
-        int d=3;
+        int d=8;
         System.out.println(a+b);//summation
     }
 }
